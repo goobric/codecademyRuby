1d47fafa9c020c1cad5815719d4c85f485805856
goobric aka Mikal <goobric@gmail.com>
2022-05-28 13:34:46 +0200

    new pet hash

--- DataStructures/hashes.rb
+++ DataStructures/hashes.rb
@@ -27,5 +27,12 @@ my_hash = Hash.new
 # 1. Use Hash.new to create a new hash called pets. Hash must be capitalized, or Ruby won’t know what you’re talking about!
 pets = Hash.new
 
+## Adding to a Hash
+
+# We can add to a hash two ways: if we created it using literal notation, we can simply add a new key-value pair directly between the curly braces. If we used Hash.new, we can add to the hash using bracket notation:
+# 2. Add a pet to your pets hash. It can be any key-value pair you like!
+
+pets = Hash.new
+pets["Pepper"] = "dog"
 
 
