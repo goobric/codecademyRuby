8b7cad48233a2213965a9d0a1283ec350b95a6cc
goobric aka Mikal <goobric@gmail.com>
2022-05-28 17:15:38 +0200

    sorting using hash

--- DataStructures/histogram.rb
+++ DataStructures/histogram.rb
@@ -80,6 +80,26 @@ puts colors["blue"]
 ##Hint if you need help.
 # The action we want is to update the hash with a key equal to the word, then increment its value by 1. Each key will be frequencies[word], and we can increment using += 1.
 
+## Sorting the Hash
+# Great! We have a hash full of word / frequency key-value pairs. Now we need to figure out a way to get our information in the order we want it.
+colors = { 
+    "blue" => 3,
+    "green" => 1,
+    "red" => 2
+  }
+  colors = colors.sort_by do |color, count|
+    count
+  end
+  colors.reverse!
+
+# In the example above, we first create a hash called colors that maps color strings to numbers.
+# Then, we sort colors into green, red, and blue, from smallest to largest by count. Just so you know, the .sort_by function returns an array of arrays, but that’s fine for our purposes.
+# Finally, we reverse the array order so that the colors with the largest counts are first.
+
+# Can you do the same thing with the frequencies hash?
+# Task: Use .sort_by to sort the frequencies hash by word count, like step 2 above. Store the result back in frequencies.
+# Use .reverse! to reverse the sorted frequencies array.
+
 
 
 
