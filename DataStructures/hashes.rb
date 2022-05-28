bc21458143a1637788b53dbb3b84a660be084835
goobric aka Mikal <goobric@gmail.com>
2022-05-28 13:39:23 +0200

    accessing key-value pair data

--- DataStructures/hashes.rb
+++ DataStructures/hashes.rb
@@ -35,4 +35,28 @@ pets = Hash.new
 pets = Hash.new
 pets["Pepper"] = "dog"
 
+## Accessing Hash Values
+
+You can access values in a hash just like an array.
+pets = {
+    "Stevie" => "cat",
+    "Bowser" => "hamster",
+    "Kevin Sorbo" => "fish"
+  }
+   
+puts pets["Stevie"]
+# will print "cat"
+
+
+# In the example above, we create a hash called pets.
+# Then we print cat by accessing the key "Stevie" in the pets hash.
+# Task: Access the key-value pair you added to pets, just like step 2 in the example above.
+# Use puts to print that value to the console.
+pets = Hash.new
+pets["Pepper"] = "dog"
+puts pets["Pepper"]
+
+
+
+
 
