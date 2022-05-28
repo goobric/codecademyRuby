1c09645b2ad97bf987b1308672dd8ff602f2b809
goobric aka Mikal <goobric@gmail.com>
2022-05-28 17:47:02 +0200

    Method Syntax

--- MethodsBlocksSorting/exMethod.rb
+++ MethodsBlocksSorting/exMethod.rb
@@ -24,4 +24,14 @@ def prime(n)
   prime(11)
   prime(51)
   prime(97)
-  
+
+### Method Syntax
+# Methods are defined using the keyword def (short for “define”). Methods have three parts:
+# The header, which includes the def keyword, the name of the method, and any arguments the method takes. (We’ll get to arguments in the next section)
+# The body, which is the code block that describes the procedures the method carries out. The body is indented two spaces by convention (as with for, if, elsif, and else statements)
+# The method ends with the end keyword.
+# Here’s an example of what the syntax would look like for a simple function, welcome, that just prints “Welcome to Ruby!” to the console:
+
+def welcome
+    puts "Welcome to Ruby!"
+end
