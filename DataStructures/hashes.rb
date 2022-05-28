73099cdc3fb1788c6457ea064dc09aad0cc46da2
goobric aka Mikal <goobric@gmail.com>
2022-05-28 13:40:47 +0200

    re-intro of Iteration

--- DataStructures/hashes.rb
+++ DataStructures/hashes.rb
@@ -56,6 +56,25 @@ pets = Hash.new
 pets["Pepper"] = "dog"
 puts pets["Pepper"]
 
+##(Re)Introduction to Iteration
+
+# Remember when we covered loops and iterators? We could use a whole bunch of different methods for looping in a program. When we loop over an array or a hash, we say that we iterate over it.
+
+# We’ll be using the .each iterator to iterate over arrays and hashes in this section. Take a look at the code in the editor to see one example of how we might go about this.
+
+friends = ["Milhouse", "Ralph", "Nelson", "Otto"]
+
+family = { "Homer" => "dad",
+  "Marge" => "mom",
+  "Lisa" => "sister",
+  "Maggie" => "sister",
+  "Abe" => "grandpa",
+  "Santa's Little Helper" => "dog"
+}
+
+friends.each { |x| puts "#{x}" }
+family.each { |x, y| puts "#{x}: #{y}" }
+
 
 
 
