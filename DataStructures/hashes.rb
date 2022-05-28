6005bd730cfdd83c4265c4057e455e89474d991a
goobric aka Mikal <goobric@gmail.com>
2022-05-28 13:51:43 +0200

    .each iterator example

--- DataStructures/hashes.rb
+++ DataStructures/hashes.rb
@@ -75,7 +75,18 @@ family = { "Homer" => "dad",
 friends.each { |x| puts "#{x}" }
 family.each { |x, y| puts "#{x}: #{y}" }
 
+##Iterating Over Arrays
+# Iterating over arrays is easier than it looks. 
+numbers = [1, 2, 3, 4, 5]
+numbers.each { |element| puts element }
 
+# In the example above, we create an array called numbers with 5 elements.
+# Then we say, “Take this array and for each element, print it to the console.” As usual, we can use any placeholder name for the bit between two | | characters.
+# Task: Use the .each iterator to puts out each element of the languages array.
+# Make sure to use puts instead of print, so each element is on its own line.
+
+languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+languages.each {|langPH| puts language}
 
 
 
