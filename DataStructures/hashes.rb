cea22327b7f04854dc967cc95b7a1723ee2bb39f
goobric aka Mikal <goobric@gmail.com>
2022-05-28 14:20:36 +0200

    puts (print) hash value

--- DataStructures/hashes.rb
+++ DataStructures/hashes.rb
@@ -179,5 +179,22 @@ btcc.each do |team, driver|
     puts "#{team}: #{driver}"
 end
 
+## Iterating Over a Hash
+#We’ve done a fair amount of iteration over arrays, so to finish up, let’s review how to iterate over a hash.
 
+numbers = [1, 2, 3, 4, 5]
+numbers.each { |element| puts element }
 
+# Task: Iterate through .each key/value pair in lunch_order.
+# Please puts out the value of each pair (just the value, not the key).
+
+lunch_order = {
+    "Ryan" => "wonton soup",
+    "Eric" => "hamburger",
+    "Jimmy" => "sandwich",
+    "Sasha" => "salad",
+    "Cole" => "taco"
+  }
+lunch_order.each do |name, food|
+    puts "#{food}"
+end
