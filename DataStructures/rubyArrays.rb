c5e91de1254f5a53ebe2bb2c3399aa1a1251ad24
goobric aka Mikal <goobric@gmail.com>
2022-05-28 12:23:34 +0200

    string array

---
+++ DataStructures/rubyArrays.rb
@@ -0,0 +1,9 @@
+# Arrays of Non-Numbers
+
+# Here’s something you might not have known: you can make an array of any collection of Ruby objects.
+# You can make an array of booleans! An array of strings! The possibilities are (almost) endless.
+
+
+# 1. Create a new array called string_array. Make it an array of… strings!
+
+string_array = ['ab', 'cd', 'ef', 'gh']
