e5e9f4af2314ebc487af03461b3a93f5f6e75832
goobric aka Mikal <goobric@gmail.com>
2022-05-28 14:17:22 +0200

    example hash

--- DataStructures/hashes.rb
+++ DataStructures/hashes.rb
@@ -152,11 +152,27 @@ end
 
 my_array = [["bmw", "winkelhock"], ["audi", "bintcliff"], ["vauxhall", "cleland"], ["ford", "rouse"], ["renault", "harvey"]]
 
+## Hashes
+# Good! Now let’s create a hash. Feel free to use either hash literal notation or Hash.new.
+
+prices = { 
+    "apple" => 0.52,
+    "banana" => 0.23,
+    "kiwi" => 1.42
+  }
+   
+sounds = Hash.new
+sounds["dog"] = "woof"
+sounds["cat"] = "meow"
+
+# Task: Create a hash called my_hash in the editor.
+# Give it at least one key-value pair.
+
 btcc = {
-    "bmw" => "winkelhock"
-    "audi" => "bincliff"
-    "vauxhall" => "cleland"
-    "ford" => "rouse"
+    "bmw" => "winkelhock",
+    "audi" => "bincliff",
+    "vauxhall" => "cleland",
+    "ford" => "rouse",
     "renault" => "harvey"
 }
 btcc.each do |team, driver|
