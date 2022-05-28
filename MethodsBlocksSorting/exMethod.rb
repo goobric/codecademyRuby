222dd3296985da72965bf940ccfb2bf442e64b6e
goobric aka Mikal <goobric@gmail.com>
2022-05-28 17:55:09 +0200

    Calling a variable

--- MethodsBlocksSorting/exMethod.rb
+++ MethodsBlocksSorting/exMethod.rb
@@ -44,3 +44,33 @@ def puts_1_to_10
 end
   
   puts_1_to_10 # Ignore this for now. We'll explain it soon!
+
+  Create Your Own
+
+Now it’s time for you to build your own method. Remember, the syntax looks like this:
+
+def method_name
+  # Do something!
+end
+
+# TASK: Create a method, greeting, in the editor.
+# It should use puts to print a greeting to the console.
+# Define your method below!
+def greeting
+    puts "Welcome to Methods Blocks & Sorting"
+end
+  
+# Define your method above this line.
+greeting # Ignore this for now. We'll explain it in the next exercise!
+
+## Call It!
+# Defining a method is great, but it’s not much use to you unless you call it, or tell your program to execute it. For example, if you call a method called cartoon_fox, the program will start looking for the method with that name and try to execute the code inside it.
+# If the program doesn’t find a method called cartoon_fox, it will return a NameError. We’ll cover errors in another lesson.
+# You call a method just by typing its name. Remember when you saw us type puts_1_to_10 or greeting after our method definition in the last two exercises? That was us calling our methods!
+
+## TASK
+# We’ve set up a function, array_of_10, in the editor to the right. Call it on line 5!
+def array_of_10
+    puts (1..10).to_a
+end
+array_of_10
