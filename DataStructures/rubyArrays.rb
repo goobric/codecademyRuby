95d2a53cec865bd64fa154d3927a722f38153770
goobric aka Mikal <goobric@gmail.com>
2022-05-28 12:30:52 +0200

    multi dimensional array

--- DataStructures/rubyArrays.rb
+++ DataStructures/rubyArrays.rb
@@ -7,3 +7,16 @@
 # 1. Create a new array called string_array. Make it an array of… strings!
 
 string_array = ['ab', 'cd', 'ef', 'gh']
+
+puts string_array
+
+# Arrays of Arrays
+
+# You might be asking yourself: “If I can put anything in an array, can I make an array of arrays?” The answer is: yes!
+
+# Check out the array of arrays we have in the editor. Arrays of arrays are called multidimensional arrays, since the act of adding more arrays expands the array out of its string-like shape. For instance, the array in the editor is a two-dimensional array.
+
+multi_d_array = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+multi_d_array.each { |x| puts "#{x}\n" }
+
