70228a48f63c36e0644939363d6fa2bca740ad67
goobric aka Mikal <goobric@gmail.com>
2022-05-30 16:51:27 +0200

    what are symbols for?

--- DataStructures/hashStory.rb
+++ DataStructures/hashStory.rb
@@ -127,3 +127,22 @@ puts :symbol.object_id
 # Create a regular old variable called my_first_symbol and set it equal to any valid symbol you like!
 
 my_first_symbol = :my_symbol
+
+## 09 What are Symbols Used For?
+
+# Symbols pop up in a lot of places in Ruby, but they’re primarily used either as hash keys or for referencing method names. (We’ll see how symbols can reference methods in a later lesson.)
+
+sounds = {
+  :cat => "meow",
+  :dog => "woof",
+  :computer => 10010110,
+}
+
+# Symbols make good hash keys for a few reasons:
+
+# They’re immutable, meaning they can’t be changed once they’re created;
+# Only one copy of any symbol exists at a given time, so they save memory;
+# Symbol-as-keys are faster than strings-as-keys because of the above two reasons.
+
+# TASK: Add at least two more keys to the symbol_hash hash replacing the ______ blanks.
+# The keys should all be symbols; their associated values can be anything you like.
