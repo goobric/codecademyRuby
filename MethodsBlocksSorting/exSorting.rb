2f33584e0a7fe71bdf512d76e6e6488bf251ef71
goobric aka Mikal <goobric@gmail.com>
2022-05-30 12:55:33 +0200

    sort example

--- MethodsBlocksSorting/exSorting.rb
+++ MethodsBlocksSorting/exSorting.rb
@@ -46,5 +46,9 @@ book_1 <=> book_2
 # Use the example of sorting in ascending order on line 4 as a guide.
 # Display the result in the console.
 
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
 
+# To sort our books in ascending order, in-place
+books.sort! { |firstBook, secondBook| firstBook <=> secondBook }
 
+# Sort your books in descending order, in-place below
