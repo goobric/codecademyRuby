7cc4cc8095d2d46e2d9ebf3563d58a35a83ecc13
goobric aka Mikal <goobric@gmail.com>
2022-05-30 14:14:20 +0200

    setting own Hash

--- DataStructures/hashStory.rb
+++ DataStructures/hashStory.rb
@@ -65,4 +65,12 @@ creatures = { "weasels" => 0,
 }
 creatures["lions"]
 
+## 05 Setting Your Own Default
 
+# You don’t have to settle for nil as a default value, however. If you create your hash using the Hash.new syntax, you can specify a default like so:
+
+my_hash = Hash.new("Trady Blix")
+
+# Now if you try to access a nonexistent key in my_hash, you’ll get "Trady Blix" as a result.
+# You can always read more hashy goodness in the official Ruby documentation
+# TASK: In the meantime, create a hash called no_nil_hash and give it any default value other than nil.
