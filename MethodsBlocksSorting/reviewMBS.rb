150d68ffeffd1937c1d61a0c761ba9ab33a89b97
goobric aka Mikal <goobric@gmail.com>
2022-05-30 12:57:52 +0200

    review Methods Blocks Sorting

---
+++ MethodsBlocksSorting/reviewMBS.rb
@@ -0,0 +1,13 @@
+## Basic Methods
+
+# Let’s quickly review how to create a basic Ruby method.
+
+def double(n)
+  return n * 2
+end
+
+# The example above is just a syntax reminder.
+# ITASK: Create a method, welcome, that puts “Welcome to Ruby!”
+# After defining your method, call it.
+
+
