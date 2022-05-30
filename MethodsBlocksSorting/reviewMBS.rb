e284d1a49bd6af3755c4178bc92d584ce1146430
goobric aka Mikal <goobric@gmail.com>
2022-05-30 13:04:38 +0200

    return solution

--- MethodsBlocksSorting/reviewMBS.rb
+++ MethodsBlocksSorting/reviewMBS.rb
@@ -21,3 +21,19 @@ def welcome
 end
 
 welcome
+
+## Methods With Arguments
+
+# Good! Now let’s make our method a bit more complex by adding arguments and a return statement.
+
+def double(n)
+  return n * 2
+end
+
+# TASK: Change your welcome method definition.
+# Add a single string parameter called name.
+# Remove your puts statement and replace it with return "Hello, #{name}"
+
+def welcome(name)
+    return "Hello, #{name}"
+end
