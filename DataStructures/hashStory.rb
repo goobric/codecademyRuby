a3147ab6048636de8c4e27dd56eb7a3c15430f83
goobric aka Mikal <goobric@gmail.com>
2022-05-30 16:49:33 +0200

    first :colon symbol

--- DataStructures/hashStory.rb
+++ DataStructures/hashStory.rb
@@ -125,3 +125,5 @@ puts :symbol.object_id
 
 # TASK: All right! Time to create your first symbol.
 # Create a regular old variable called my_first_symbol and set it equal to any valid symbol you like!
+
+my_first_symbol = :my_symbol
