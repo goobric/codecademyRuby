fbb1f3092d1b98ef29e6586b37d0211746932d21
goobric aka Mikal <goobric@gmail.com>
2022-05-30 13:05:52 +0200

    Blocks review

--- MethodsBlocksSorting/reviewMBS.rb
+++ MethodsBlocksSorting/reviewMBS.rb
@@ -37,3 +37,18 @@ end
 def welcome(name)
     return "Hello, #{name}"
 end
+
+## Blocks Review
+
+# Let’s go over what we learned about blocks.
+
+numbers = [5, 2, 8]
+sum = 0
+numbers.each do |n|
+  sum += n
+end
+puts sum
+
+# The example above is just a reminder about syntax. 
+# We calculate the sum of a list of numbers.
+# TASK: Add a block after .each that multiplies each item by itself and puts the result to the console.
