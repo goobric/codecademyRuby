1f867eb94924fe7757cf353d82a7be7ea71de576
goobric aka Mikal <goobric@gmail.com>
2022-05-30 13:31:04 +0200

    Default Parameters

--- MethodsBlocksSorting/libraryBuild.rb
+++ MethodsBlocksSorting/libraryBuild.rb
@@ -17,3 +17,27 @@ books = ["Heart of Darkness", "Code Complete", "The Lorax", "The Prophet", "Absa
 puts "A-Z: #{alphabetize(books)}"
 puts "Z-A: #{alphabetize(books, true)}"
 
+def alphabetise(arr, rev=false)
+end
+
+## 01 Defining Our Method
+# First, we’ll need to define our alphabetize method. LINE 20
+# TASK: Define alphabetize in the editor above LINE 20
+# It shouldn’t take any arguments yet, and there doesn’t need to be anything in the body of the method.
+
+## 02 Default Parameters
+
+# Let’s start with the new bit of code you saw in exercise 1:
+
+def alphabetize(arr, rev=false)
+
+# The first part makes sense—we’re defining a method, alphabetize.
+# We can guess that the first parameter is an array, but what’s this rev=false business?
+# What this does is tell Ruby that alphabetize has a second parameter, rev (for “reverse”) that will default to false if the user doesn’t type in two arguments.
+# You might have noticed that our first call to alphabetize in exercise 1 was just
+
+alphabetize(books)
+
+# Ruby didn’t see a rev, so it gave it the default value of false.
+# TASK: Update your alphabetize method with two parameters: arr, representing the array to be passed in, and rev, a boolean that defaults to false.
+# Update LINE 20
