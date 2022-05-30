66392bfbb73d8dfad289f1eee38136da4f410122
goobric aka Mikal <goobric@gmail.com>
2022-05-30 14:16:34 +0200

    no NIL Hash

--- DataStructures/hashStory.rb
+++ DataStructures/hashStory.rb
@@ -74,3 +74,4 @@ my_hash = Hash.new("Trady Blix")
 # Now if you try to access a nonexistent key in my_hash, you’ll get "Trady Blix" as a result.
 # You can always read more hashy goodness in the official Ruby documentation
 # TASK: In the meantime, create a hash called no_nil_hash and give it any default value other than nil.
+no_nil_hash = Hash.new("I am not NIL")
