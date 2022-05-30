957a22090709e0083532f16eaa6d9a3bf9f71ad6
goobric aka Mikal <goobric@gmail.com>
2022-05-30 13:15:37 +0200

    task solution

--- MethodsBlocksSorting/reviewMBS.rb
+++ MethodsBlocksSorting/reviewMBS.rb
@@ -78,3 +78,8 @@ end
 # Remember that the above example was how we sorted in alphabetical order.
 # TASK: Use .sort! to sort the fruits array in descending (that is, reverse) alphabetical order.
 # You can use the combined comparison operator (like the example above) or an if/elsif/else statement.
+
+fruits = ["orange", "apple", "banana", "pear", "grapes"]
+fruits.sort! do |firstFruit, secondFruit|
+    secondFruit <=> firstFruit
+end
