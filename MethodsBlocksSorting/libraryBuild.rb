254f17834eab65e53a4e92e2707be3c5cda7a749
goobric aka Mikal <goobric@gmail.com>
2022-05-30 13:21:19 +0200

    Library Build Task

---
+++ MethodsBlocksSorting/libraryBuild.rb
@@ -0,0 +1,7 @@
+## Building a Library
+
+# We noticed in the last lesson that .sort! didn’t have a built-in way of handling sorting in reverse alphabetical order. Now that we know how to write our own Ruby methods, we can fix that!
+# TASK: The code in the editor should look familiar—there’s only one new idea (on line 1).
+# When you’re ready, click Run to execute the code and start building your own method.
+
+
