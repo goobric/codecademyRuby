273251cb7203a282eea084bfdc7ba20e0da99d3f
goobric aka Mikal <goobric@gmail.com>
2022-05-30 13:11:04 +0200

    my solution

--- MethodsBlocksSorting/reviewMBS.rb
+++ MethodsBlocksSorting/reviewMBS.rb
@@ -52,3 +52,11 @@ puts sum
 # The example above is just a reminder about syntax. 
 # We calculate the sum of a list of numbers.
 # TASK: Add a block after .each that multiplies each item by itself and puts the result to the console.
+
+my_array = [1, 2, 3, 4, 5]
+
+my_array.each do |n|
+  my_array = n^2
+end
+
+puts my_array
