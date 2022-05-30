dffed7f71a0603e44378461502f1d28ac7cfaf4e
goobric aka Mikal <goobric@gmail.com>
2022-05-30 14:09:27 +0200

    NIL introduction

--- DataStructures/hashStory.rb
+++ DataStructures/hashStory.rb
@@ -34,4 +34,35 @@ my_hash.each do |key, value|
 end
 
 # This will print out a list of values from my_hash, each on its own line.
-# TASK: Iterate over the matz hash and print each value to the console using puts.
+# TASK: Iterate over the matz hash and print each value to the console using puts. LINE 44
+matz = { "First name" => "Yukihiro",
+    "Last name" => "Matsumoto",
+    "Age" => 47,
+    "Nationality" => "Japanese",
+    "Nickname" => "Matz"
+}
+matz.each do |key, value|
+    puts value
+end
+
+## 03 Nil: a Formal Introduction
+
+# What happens if you try to access a key that doesn’t exist, though?
+
+# In many languages, you’ll get an error of some kind. Not so in Ruby: you’ll instead get the special value nil.
+
+# Along with false, nil is one of two non-true values in Ruby. (Every other object is regarded as “truthy,” meaning that if you were to type if 2 or if "bacon", the code in that if statement would be run.)
+
+# It’s important to realize that false and nil are not the same thing: false means “not true,” while nil is Ruby’s way of saying “nothing at all.”
+# TASK: Go ahead and try to access a key in creatures that doesn’t exist.
+
+creatures = { "weasels" => 0,
+    "puppies" => 6,
+    "platypuses" => 3,
+    "canaries" => 1,
+    "Heffalumps" => 7,
+    "Tiggers" => 1
+}
+
+
+
