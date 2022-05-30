37328e27478c148fc45b3857cfb1e1c67c951096
goobric aka Mikal <goobric@gmail.com>
2022-05-30 13:22:12 +0200

    initial code

--- MethodsBlocksSorting/libraryBuild.rb
+++ MethodsBlocksSorting/libraryBuild.rb
@@ -4,4 +4,16 @@
 # TASK: The code in the editor should look familiar—there’s only one new idea (on line 1).
 # When you’re ready, click Run to execute the code and start building your own method.
 
+def alphabetize(arr, rev=false)
+    if rev
+      arr.sort { |item1, item2| item2 <=> item1 }
+    else
+      arr.sort { |item1, item2| item1 <=> item2 }
+    end
+end
+  
+books = ["Heart of Darkness", "Code Complete", "The Lorax", "The Prophet", "Absalom, Absalom!"]
+  
+puts "A-Z: #{alphabetize(books)}"
+puts "Z-A: #{alphabetize(books, true)}"
 
