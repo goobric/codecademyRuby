2be36df7c709cc60dd2337a56d2d2fadab2c67f0
goobric aka Mikal <goobric@gmail.com>
2022-05-31 18:27:24 +0200

    next task

--- MethodsBlocksSorting/movieNight.rb
+++ MethodsBlocksSorting/movieNight.rb
@@ -89,3 +89,28 @@ answer = gets.chomp
 # TASK:
 # Create a hash and assign it to the variable movies. Put in your favorite movie as the key, with a number rating for the value.
 # Prompt the user for input with puts and save that input in a variable called choice.
+
+### The Case Statement
+
+# Good work! Now we’ll want to create the main body of our program: the case statement, which will decide what actions to take based on what the user types in.
+# if and else are powerful, but we can get bogged down in ifs and elsifs if we have a lot of conditions to check. Thankfully, Ruby provides us with a concise alternative: the case statement. The syntax looks like this:
+
+case language
+  when "JS"
+    puts "Websites!"
+  when "Python"
+    puts "Science!"
+  when "Ruby"
+    puts "Web apps!"
+  else
+    puts "I don't know!"
+end
+
+# The else is what the case statement will do if it doesn’t match any of its when statements to the case (in this case, the value of language).
+Below your existing code, create a case statement for the choice variable with the following when conditions:
+
+    when "add", please puts "Added!"
+    when "update", please puts "Updated!"
+    when "display", please puts "Movies!"
+    when "delete", please puts "Deleted!"
+    Otherwise (i.e. else), please puts "Error!"Don’t forget the end statement after your case/when lines.
