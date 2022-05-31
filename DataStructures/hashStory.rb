8b84f318226b3426628da6f6ed500bddb81540d1
goobric aka Mikal <goobric@gmail.com>
2022-05-31 17:58:22 +0200

    more Method solution

--- DataStructures/hashStory.rb
+++ DataStructures/hashStory.rb
@@ -358,3 +358,19 @@ my_hash.each_value { |v| print v, " " }
 
 # Let’s wrap up our study of Ruby hashes and symbols by testing these methods out.
 # TASK: Go ahead and print out just the titles of our movies using puts.
+
+movie_ratings = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 3,
+  truman_show: 4,
+  red_dawn: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
+# Add your code below!
+
+# movie_ratings.each_key {|k| print k, " "}
+movie_ratings.each_key { |title| puts title }
