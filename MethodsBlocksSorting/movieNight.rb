967e3dda15873ef77480b3cb52fe26f9f2ec760e
goobric aka Mikal <goobric@gmail.com>
2022-05-31 19:11:26 +0200

    symbol & integars

--- MethodsBlocksSorting/movieNight.rb
+++ MethodsBlocksSorting/movieNight.rb
@@ -140,5 +140,12 @@ numbers["one"] = 1
 puts 3 + numbers["one"]
 # prints out "4"
 
+### Not My Type
 
+# Perfect! Our program is really coming along.
+# You might have wondered how we’re going to get our movies and ratings from the user—which come in as strings—into the hash where we want our movies to be symbols and our ratings to be integers. Built-in Ruby magic to the rescue!
+# Ruby’s .to_sym method can convert a string to a symbol, and .to_i will convert a string to an integer.
+# TASK: Call .to_sym on your title and .to_i on your rating so that your movie titles are stored as symbols in the hash and the associated ratings are stored as integers.
+# HINT
+# You can also use .intern instead of .to_sym, if you like.
         
