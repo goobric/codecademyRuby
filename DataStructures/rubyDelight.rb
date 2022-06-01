b89ca3844b831cefbaf019a7ed4aac6f2c888b26
goobric aka Mikal <goobric@gmail.com>
2022-06-01 16:38:07 +0200

    ways of writing IF

--- DataStructures/rubyDelight.rb
+++ DataStructures/rubyDelight.rb
@@ -4,3 +4,43 @@
 
 # Check out the code in the editor. It looks almost like English, doesn’t it?
 
+ruby_is_eloquent = true
+ruby_is_ugly = false
+
+puts "Ruby is eloquent!" if ruby_is_eloquent
+puts "Ruby's not ugly!" unless ruby_is_ugly
+
+# Output
+# Ruby is eloquent!
+# Ruby's not ugly!
+
+## A Simpler 'If'
+
+# You’ve seen the Ruby if statement before:
+
+if condition
+  # Do something!
+end
+
+# If the “do something” is a short, simple expression, however, we can move it up into a single line (as you saw in the last exercise). The syntax looks like this:
+
+expression if boolean
+
+# Ruby will expect an expression followed by if followed by a boolean. The order is important. You can do this:
+
+puts "It's true!" if true
+
+# but not this:
+
+if true puts "It's true!"
+
+# It’s also important to note that you don’t need an end when you write your if statement all on one line.
+# TASK: Give it a go! Type a one-line if statement in the editor to the right. It can be whatever you like.
+# You can try a simple one-liner, like this:
+puts "Hello!" if true
+
+# which is the same as:
+rb if true puts "Hello!" end
+
+# Type your Ruby code below!
+puts "Hello coders!" if true
