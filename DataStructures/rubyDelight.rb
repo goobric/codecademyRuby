eac90b212d741f94e7ac80140c29ce7c9be674cd
goobric aka Mikal <goobric@gmail.com>
2022-06-01 17:13:02 +0200

    Conditional Assignment

--- DataStructures/rubyDelight.rb
+++ DataStructures/rubyDelight.rb
@@ -130,3 +130,14 @@ case greeting
   when "Finnish" then puts "Haloo!"
   else puts "I don't know that language!"
 end
+
+## Conditional Assignment
+
+# We’ve seen that we can use the = operator to assign a value to a variable. But what if we only want to assign a variable if it hasn’t already been assigned?
+
+# For this, we can use the conditional assignment operator: ||=. It’s made up of the or (||) logical operator and the normal = assignment operator.
+
+# Run the code in the editor. Here’s what’s happening:
+# favorite_book is set to nil, which is Ruby for “nothing.” When you try to puts it to the screen, you get exactly that: nothing!
+# Now our variable is conditionally set to “Cat’s Cradle.” Since the value of the variable was nothing before, Ruby goes ahead and sets it, so you see “Cat’s Cradle” printed out.
+# We try conditional assignment again, this time with “Why’s (Poignant) Guide to Ruby.” But wait! Our variable already has a value, “Cat’s Cradle,” so it stays set to that value and that’s what we see printed out.
