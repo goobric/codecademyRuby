95b21b91846ba30b419afa4156a4b83b04ce2c37
goobric aka Mikal <goobric@gmail.com>
2022-06-04 17:14:12 +0200

    solution If Unless

--- DataStructures/refactorFactory.rb
+++ DataStructures/refactorFactory.rb
@@ -47,4 +47,14 @@ puts "n must be an integar" unless n.is_a?
  
 puts "n must be greater than 0." if n <= 0
 
+# Solution
+require 'prime'   # This is a module. We'll cover these soon!
+
+def first_n_primes(n)
+  return "n must be an integer." unless n.is_a? Integer
+  return "n must be greater than 0." if n <= 0
+  return Prime.first n
+end
+
+first_n_primes(10)
 
