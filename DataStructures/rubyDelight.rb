6de10297901d423ec293e190d9b63468f7bb7968
goobric aka Mikal <goobric@gmail.com>
2022-06-04 14:39:58 +0200

    string interpolation solution

--- DataStructures/rubyDelight.rb
+++ DataStructures/rubyDelight.rb
@@ -330,3 +330,12 @@ age = 26
 # ==> I am 26 years old.
   
 # All you need to do is place the variable name inside #{} within a string!
+# TASK: Remove the concatenation operator and rewrite the code to use #{thing}.You will want to puts one complete string on line 6.
+
+favorite_things = ["Ruby", "espresso", "candy"]
+
+puts "A few of my favorite things:"
+
+favorite_things.each do |thing|
+  puts "I love #{thing}!"
+end
