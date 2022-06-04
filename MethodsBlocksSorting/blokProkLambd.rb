01eb89760cacc48ba644b836ca84eafefff72b82
goobric aka Mikal <goobric@gmail.com>
2022-06-04 17:37:18 +0200

    using the Yield key word

--- MethodsBlocksSorting/blokProkLambd.rb
+++ MethodsBlocksSorting/blokProkLambd.rb
@@ -51,7 +51,16 @@ fibs = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 doubled_fibs = fibs.collect { |x| x * 2 }
 puts doubled_fibs
 
-Learning to Yield
-Why do some methods accept a block and others don’t? It’s because methods that accept blocks have a way of transferring control from the calling method to the block and back again. We can build this into the methods we define by using the yield keyword.
-TASK: Check out the code in the editor. We’ve set up a series of puts statements so you can see when we’re in the block_test method and when we’re in the block passed to it. Click Run to see the result!
+### Learning to Yield
+# Why do some methods accept a block and others don’t? It’s because methods that accept blocks have a way of transferring control from the calling method to the block and back again. We can build this into the methods we define by using the yield keyword.
+# TASK: Check out the code in the editor. We’ve set up a series of puts statements so you can see when we’re in the block_test method and when we’re in the block passed to it. Click Run to see the result!
+
+def block_test
+    puts "We're in the method!"
+    puts "Yielding to the block..."
+    yield
+    puts "We're back in the method!"
+end
+  
+block_test { puts ">>> We're in the block!" }
 
