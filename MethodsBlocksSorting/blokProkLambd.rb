d89760e319fe2e9afe2f665d11f8ee8f8c52da87
goobric aka Mikal <goobric@gmail.com>
2022-06-04 17:28:18 +0200

    task with times

--- MethodsBlocksSorting/blokProkLambd.rb
+++ MethodsBlocksSorting/blokProkLambd.rb
@@ -12,3 +12,8 @@ end
    
 [1, 2, 3].each { |num| puts num }
 # ==> Prints 1, 2, 3 on separate lines
+# Blocks can be combined with methods like .each and .times to execute an instruction for each element in a collection (like a hash or array).
+# TASK: Let’s do a little review! Use .times and a block to puts the string “I’m a block!” five times.
+
+# Write your code below!
+5.times {puts "I'm a block!"}
