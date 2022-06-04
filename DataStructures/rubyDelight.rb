00ec5df687e171fec565987c2a89523a15408f73
goobric aka Mikal <goobric@gmail.com>
2022-06-04 14:42:27 +0200

    One Liner task

--- DataStructures/rubyDelight.rb
+++ DataStructures/rubyDelight.rb
@@ -320,7 +320,7 @@ age = 26
 # ==> "I am 26 years old."
 "I am " << age.to_s << " years old."
 # ==> "I am 26 years old."
-
+ 
 # This is complicated, and complicated is not the Ruby way.
 # A better way to do this is with string interpolation. The syntax looks like this:
 
@@ -338,4 +338,17 @@ puts "A few of my favorite things:"
 
 favorite_things.each do |thing|
   puts "I love #{thing}!"
+end
+
+## One-Liners
+# Time to put your new knowledge to work by refactoring some existing code. Refactoring is just a fancy way of saying we’re improving the structure or appearance of our code without changing what it actually does.
+# We won’t cover every bit of syntax from the previous exercises, but we’ll hit the highlights. Ready?
+# TASK: Refactor the contents of the editor to just one line of code.
+# HINT: Remember, you can convert an if statement to a single line by using this structure:
+
+action if boolean
+
+# Where action is the action to perform if the boolean expression evaluates to true.
+if 1 < 2
+  puts "One is less than two!"
 end
