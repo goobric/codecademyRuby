1d1afac8225fa6b076378b3331173beec4f16f0b
goobric aka Mikal <goobric@gmail.com>
2022-06-06 21:48:31 +0200

    task Proc

--- MethodsBlocksSorting/blokProkLambd.rb
+++ MethodsBlocksSorting/blokProkLambd.rb
@@ -180,3 +180,29 @@ can_ride_3 = group_3.select { |height| height >= 4 }
 puts can_ride_1
 puts can_ride_2
 puts can_ride_3
+
+### Create Your Own!
+# Okay! Time to take off the training wheels.
+
+cube = Proc.new { |x| x ** 3 }
+[1, 2, 3].map(&cube)
+# [1, 8, 27]
+
+# You’re going to create your very own method that calls your very own proc! We’ll do this in two steps. Use the example above as a syntax reminder.
+# TASK:
+# Create a method, greeter, that takes no arguments and yields to a block.
+# Create a Proc, phrase, that puts "Hello there!". Pass this to greeter instead of a block. (Don’t forget to pass &phrase instead of just phrase!)
+
+## HINT:
+# Your method should look something like this:
+
+def method_name
+  yield
+end
+
+And your proc should look something like this:
+
+proc_name = Proc.new do
+  puts "Text"
+end
+
