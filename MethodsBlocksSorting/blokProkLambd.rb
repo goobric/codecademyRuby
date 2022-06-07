8893023d64c487dfbd628da9e20f4a0312986080
goobric aka Mikal <goobric@gmail.com>
2022-06-07 14:11:42 +0200

    Lambda example

--- MethodsBlocksSorting/blokProkLambd.rb
+++ MethodsBlocksSorting/blokProkLambd.rb
@@ -276,3 +276,9 @@ Proc.new { puts "Hello!" }
 # In the example to the right, when we pass the lambda to lambda_demo, the method calls the lambda and executes its code.
 # Now that you know how blocks and procs work, you should be able to guess what this lambda will puts to the console.
 
+def lambda_demo(a_lambda)
+  puts "I'm the method!"
+  a_lambda.call
+end
+
+lambda_demo(lambda { puts "I'm the lambda!" })
