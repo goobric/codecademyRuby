db6d499035c7a7842b4706312ffa0cd01bc707f0
goobric aka Mikal <goobric@gmail.com>
2022-06-07 15:13:46 +0200

    Prod vs Lambda 4

--- MethodsBlocksSorting/blokProkLambd.rb
+++ MethodsBlocksSorting/blokProkLambd.rb
@@ -341,10 +341,30 @@ puts batman_ironman_lambda
 # You’ve written your own lambda and seen how to pass it to a method. Now it’s time for you to write a lambda and pass it to a method!
 # If you think this will be a lot like what you’ve already done with procs, you’re exactly right. Just like with procs, we’ll need to put & at the beginning of our lambda name when we pass it to the method, since this will convert the lambda into the block the method expects.
 # That symbolize lambda was pretty cool. Let’s riff on it with a lambda that checks to see if each element in an array is a symbol. We can do this checking with the .is_a? method, which returns true if an object is the type of object named and false otherwise:
+## TASK:
+# Create a lambda, symbol_filter, that takes one parameter and checks to see if that parameter .is_a? Symbol.
+# Create a new variable called symbols, and store the result of calling my_array.select and passing it your lambda.
+# puts symbols at the end of the file in order to see the final contents of the array.
+# HINT:
+# Your code should look something like this:
 
-:hello.is_a? Symbol
-# ==> true
+symbol_filter = lambda { |x| x.magic! }
+my_array.select(&lambda_name)
 
-The word Symbol has to be capitalized when you’re doing an .is_a? check!
+# where x.magic! is where you check to see if each element of the array is a symbol, and lambda_name is the lambda you’re creating.
+
+my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
+
+# Add your code below!
+# symbol_filter = lambda { |x| x.is_a? }
+# my_array.select(&symbols)
+
+symbol_filter = lambda {|x| x.is_a? Symbol}
+symbols = my_array.select(&symbol_filter)
+
+my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
+
+puts symbols
+
 
 
