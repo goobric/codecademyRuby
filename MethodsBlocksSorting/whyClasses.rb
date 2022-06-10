d93e8d41f013df464febd4a7361f0319c5d8fb88
goobric aka Mikal <goobric@gmail.com>
2022-06-10 12:41:41 +0200

    using a One Liner

--- MethodsBlocksSorting/whyClasses.rb
+++ MethodsBlocksSorting/whyClasses.rb
@@ -306,3 +306,11 @@ rb def fight
     puts "Instead of breathing fire..."
     super
 end
+
+## There Can Be Only One!
+# Any given Ruby class can have only one superclass. Some languages allow a class to have more than one parent, which is a model called multiple inheritance. This can get really ugly really fast, which is why Ruby disallows it.
+# However, there are instances where you want to incorporate data or behavior from several classes into a single class, and Ruby allows this through the use of mixins. We’ll learn about mixins in a later lesson! For now, we’ll demonstrate what happens if you try to do multiple inheritance in Ruby.
+# The demo code we’re about to show you includes a fancy trick: if you want to end a Ruby statement without going to a new line, you can just type a semicolon. This means you can write something like
+class Monkey
+end
+# on just one line: class Monkey; end. This is a time saver when you’re writing something very short, like an empty class or method definition.
