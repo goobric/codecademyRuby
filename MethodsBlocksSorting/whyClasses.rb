4931f2ae67e80df3c3ffd0913e1af3aa2c32f95a
goobric aka Mikal <goobric@gmail.com>
2022-06-10 11:49:50 +0200

    Importance of Classes & Objects

--- MethodsBlocksSorting/whyClasses.rb
+++ MethodsBlocksSorting/whyClasses.rb
@@ -193,3 +193,22 @@ matz = Person.new("Yukihiro")
 dhh = Person.new("David")
   
 puts "Number of Person instances: #{Person.number_of_instances}"
+
+## Classes Are Serious Business
+# Classes like Language and Person are great when you are starting to learn the concepts of classes and instances.
+# However, classes and objects are often used to model real-world objects.
+# The code in the editor is a more realistic demonstration of the kind of classes and objects you might find in commercial software.
+# Here we have a snippet of the Rails source code.
+# See how they’ve created an instance of the RecordInvalid class?
+
+# TASK
+# Most of the syntax should look familiar to you; the raise bit (which we’ll cover in future lessons) generates a new RecordInvalid error if the user tries to create or save an invalid record.
+def create_record(attributes, raise_error = false)
+    record = build_record(attributes)
+    yield(record) if block_given?
+    saved = record.save
+    set_new_record(record)
+    raise RecordInvalid.new(record) if !saved && raise_error
+    record
+end
+
