1ba050aab3a9f3013993e6b7704b74c176595ae1
goobric aka Mikal <goobric@gmail.com>
2022-06-10 11:01:47 +0200

    Name parameter

--- MethodsBlocksSorting/whyClasses.rb
+++ MethodsBlocksSorting/whyClasses.rb
@@ -50,3 +50,24 @@ end
 ## HINT: Your Person class should include an initialize method, like so:
 rb def initialize end
 
+## What's in a @name?
+# All right! Just one more step before we can create a person from our Person class: we have to make sure each person has a @name.
+# In Ruby, we use @ before a variable to signify that it’s an instance variable. This means that the variable is attached to the instance of the class. 
+class Car
+    def initialize(make, model) 
+      @make = make
+      @model = model
+    end
+end
+   
+kitt = Car.new("Pontiac", "Trans Am")
+
+# The code in the example above creates an instance, kitt, of the class Car. kitt has his own @make (“Pontiac”) and @model (“Trans Am”). Those variables belong to the kitt instance, which is why they’re called instance variables.
+# We’ll cover other kinds of variables (like those attached to entire classes!) in the next few exercises.
+# TASK: LINE 42
+# Give your initialize method a single parameter, name. In the body of your method, set @name = name.
+# This tells Ruby that whenever it creates a Person, it has to have a name, and each instance of Person will have its own @name.
+# HINT:
+# Keeping parameters and instance variables straight can be tricky, so here’s a freebie. Your initialize method should look like this:
+
+rb def initialize(name) @name = name end
