58fb5c8f82e16e36fb0cf47e5a729d983db4912b
goobric aka Mikal <goobric@gmail.com>
2022-06-10 12:32:23 +0200

    DerivedClass & SubClass

--- MethodsBlocksSorting/whyClasses.rb
+++ MethodsBlocksSorting/whyClasses.rb
@@ -281,3 +281,17 @@ class Dragon < Creature
         return "Breathes fire!"
     end
 end
+
+## DerivedClass & SubClass
+# On the flip side, sometimes you’ll be working with a derived class (or subclass) and realize that you’ve overwritten a method or attribute defined in that class’ base class (also called a parent or superclass) that you actually need.
+# Have no fear! You can directly access the attributes or methods of a superclass with Ruby’s built-in super keyword.
+# The syntax looks like this:
+class DerivedClass < BaseClass
+    def some_method
+        super(optional arguments)
+            # Some function
+        end
+    end
+end
+# When you call super from inside a method, that tells Ruby to look in the superclass of the current class and find a method with the same name as the one from which super is called.
+# If it finds it, Ruby will use the superclass’ version of the method.
