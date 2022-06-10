4c802d9c4340118c19ff3763a8a6a0e0fb8ff238
goobric aka Mikal <goobric@gmail.com>
2022-06-10 10:40:46 +0200

    solution Pass Lambda to Mehod

--- MethodsBlocksSorting/blokProkLambd.rb
+++ MethodsBlocksSorting/blokProkLambd.rb
@@ -457,5 +457,25 @@ crew = {
 # first_half = lambda {|k,v| <"M"}
 first_half = lambda { |x,y| y<"M"}
 
+### pass our lambda to a method, as well.
+#TASK:
+# To finish up, let’s go ahead and create a variable called a_to_m and set it equal to calling .select on crew, and pass in your first_half lambda to filter for the names that are before “M” in the alphabet. Remember to pass &first_half to convert your lambda to a block!
 
+puts a_to_m at the end of the file in order to see the final contents of the array.
 
+crew = {
+  captain: "Picard",
+  first_officer: "Riker",
+  lt_cdr: "Data",
+  lt: "Worf",
+  ensign: "Ro",
+  counselor: "Troi",
+  chief_engineer: "LaForge",
+  doctor: "Crusher"
+}
+# Add your code below!
+first_half = lambda { |x,y| y < "M"}
+
+a_to_m = crew.select(&first_half)
+
+puts a_to_m
