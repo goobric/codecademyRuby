b3f9bfc81f2b40d958a9ecc32e7ddd90aa03939b
goobric aka Mikal <goobric@gmail.com>
2022-06-14 17:56:17 +0200

    Module Name ex MyLibary

--- MethodsBlocksSorting/rubyPerson.rb
+++ MethodsBlocksSorting/rubyPerson.rb
@@ -182,4 +182,7 @@ end
 # Ruby doesn’t make you keep the same value for a constant once it’s initialized, but it will warn you if you try to change it. Ruby constants are written in ALL_CAPS and are separated with underscores if there’s more than one word.
 # An example of a Ruby constant is PI, which lives in the Math module and is approximately equal to 3.141592653589793. We created our own PI in the previous exercise, but don’t worry: because they’re in separate modules, Ruby knows to keep them separate.
 # Create your own module called MyLibrary in the editor to the right. Include a constant called FAVE_BOOK and set it equal to a string naming your favorite book!
+module MyLibrary
+    FAVE_BOOK = "Dirty Beasts"
+end
 
