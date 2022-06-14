49b47e39b063f4b1befa710c5d7d53b58e9f20c3
goobric aka Mikal <goobric@gmail.com>
2022-06-14 18:03:20 +0200

    task8 Require command

--- MethodsBlocksSorting/rubyPerson.rb
+++ MethodsBlocksSorting/rubyPerson.rb
@@ -193,3 +193,11 @@ end
 # Use the scope resolution operator to puts the value of PI from the Math module to the console.
 # To get to PI in Math, you’ll just need Math::PI!
 puts Math::PI
+
+# TASK8 REQUIRE command for Modules
+# Some modules, like Math, are already present in the interpreter. Others need to be explicitly brought in, however, and we can do this using require. We can do this simply by typing
+
+require 'module'
+    
+# We want to use the Ruby Date module to show today’s date, but we haven’t required it yet!
+# Go ahead and require 'date' on line 1 so the code on line 3 will work.
