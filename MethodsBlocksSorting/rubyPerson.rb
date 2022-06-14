6a7e79b71c223115cb7060fc6def37bab4a82aa4
goobric aka Mikal <goobric@gmail.com>
2022-06-14 18:10:54 +0200

    defining a Method in the Action module & use it within Rabbit & Cricket classes

--- MethodsBlocksSorting/rubyPerson.rb
+++ MethodsBlocksSorting/rubyPerson.rb
@@ -243,6 +243,19 @@ class Rabbit
     @name = name
   end
 end
+class Cricket
+    include Action
+    attr_reader :name
+    def initialize(name)
+      @name = name
+    end
+end
+  
+peter = Rabbit.new("Peter")
+jiminy = Cricket.new("Jiminy")
+  
+peter.jump
+jiminy.jump
 
     
     
