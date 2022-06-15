27eb59a685c6f1f242ac8b42a25538108c9b96ac
goobric aka Mikal <goobric@gmail.com>
2022-06-15 15:43:17 +0200

    task12 solution

--- MethodsBlocksSorting/rubyPerson.rb
+++ MethodsBlocksSorting/rubyPerson.rb
@@ -306,5 +306,8 @@ class Application
     attr_accessor :status
     def initialize; end
     # Add your method here!
-    
+    public
+    def print_status
+        puts "All systems go!"
+    end
 end
