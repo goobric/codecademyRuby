1391c1b596e807fc26ed40c7cb74be81048bbbc8
goobric aka Mikal <goobric@gmail.com>
2022-06-15 16:44:42 +0200

    task2 completed

--- MethodsBlocksSorting/rubyBank.rb
+++ MethodsBlocksSorting/rubyBank.rb
@@ -29,7 +29,7 @@ class Account
   end
   
   def pin_error
-    "Access denied: incorrect PIN."
+    return "Access denied: incorrect PIN."
   end
 end
 my_account = Account.new("Eric", 1_000_000)
@@ -54,4 +54,18 @@ class Account2
     @name = name
     @balance = balance
     end
+    private
+  
+  def pin
+    @pin = 1234
+  end
+  
+  def pin_error
+    return "Access denied: incorrect PIN."
+  end
 end
+
+# TASK2 Private Method
+Add two private methods to your Account class, pin and pin_error.pin should take no arguments and set an instance variable, @pin, equal to the PIN number 1234.
+pin_error should take no arguments and should simply return the string "Access denied: incorrect PIN."
+
